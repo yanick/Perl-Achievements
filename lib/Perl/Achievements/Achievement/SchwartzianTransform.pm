257f5e01b18cded6810b406ac77edc7829315529
Daniel B <daniel.bruder@gmx.net>
2012-05-30 18:18:29 +0200

    version to pull from

--- lib/Perl/Achievements/Achievement/SchwartzianTransform.pm
+++ lib/Perl/Achievements/Achievement/SchwartzianTransform.pm
@@ -1,10 +1,15 @@
 package Perl::Achievements::Achievement::SchwartzianTransform;
+
 # ABSTRACT: A new achievement!
+
 use strict;
 use warnings;
+
 use Moose;
 use MooseX::SemiAffordanceAccessor;
+
 with 'Perl::Achievements::Achievement';
+
 has transformations => (
     traits  => [qw/ Counter Perl::Achievements::Role::ConfigItem /],
     isa     => 'Num',
@@ -15,9 +20,12 @@ has transformations => (
         inc_transformations => 'inc',
     },
 );
+
 sub scan {
     my $self = shift;
+    
     use List::MoreUtils 'uniq';
+    
     # Living up to the name of...
     my $schwartzian_transformations =
       scalar                                                                                # Step 6) Superfluous but still nice to have a step 6 ;-)
@@ -27,7 +35,7 @@ sub scan {
                     !(                                                                      #   except
                             $_[1]->isa('PPI::Token::Word')                                  #   the tokens
                         and $_[1]->literal ~~ [ 'map', 'grep', 'sort' ]                     #   'map' and 'sort' ('grep' doesn't hurt here and you could
-                    )                                                                       #   do some clever combination of Steps 1 + 4 + 5....
+                    )                                                                       #   do some clever combination of Steps 1 + 4 + 5....)
             }                                                                               #   Finally,
           ) => $_->content                                                                  #   don't forget to return the content
       }                                                                                     #   (since prune only returns the number it has pruned)
@@ -37,13 +45,17 @@ sub scan {
         $self->ppi->find(                                                                   #   all places where 'map' or 'sort' occurs
             sub {                                                                           #   of course...
                       $_[1]->isa('PPI::Token::Word')                                        #   you could watch out for more
-                  and $_[1]->literal ~~ [ 'map', 'grep' ]                                   #   , 'sort', 'uniq'
+                  and $_[1]->literal ~~ [ 'map', 'sort' ]                                   #   'grep', 'uniq', ...
             }
         )
       };
+      
     $self->inc_transformations($schwartzian_transformations // 0);
+    
     return unless $self->transformations >= 1** $self->level;
+    
     $self->inc_level;
+    
     $self->unlock(
         sprintf "Nice one! You have used %d schwartzian transformations all in all throughout your scans",
                 $self->transformations
